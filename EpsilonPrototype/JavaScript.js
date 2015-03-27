8c45b0c6d803306c5142817ecfcd7ea39e842404
juan871003 <juan871003@gmail.com>
2015-03-28 03:08:15 +1100

    added pictures and basic drag drop functionality

---
+++ EpsilonPrototype/JavaScript.js
@@ -0,0 +1,15 @@
+﻿window.onload = function() {
+    $('.image.draggable').draggable({
+        containment: '#imagesTable',
+        start: dragStarted,
+        stop: dragStopped
+    });
+}
+
+function dragStarted(event, ui) {
+
+}
+
+function dragStopped(event, ui) {
+
+}
